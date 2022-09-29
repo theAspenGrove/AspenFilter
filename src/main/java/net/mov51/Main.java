6f21b3b89fa77a36232867ad9815e57fc3fb634f
mov <mov51@mov51.net>
2022-09-29 04:19:58 -0400

    Can read and replace

--- src/main/java/net/mov51/Main.java
+++ src/main/java/net/mov51/Main.java
@@ -1,7 +1,35 @@
 package net.mov51;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        ReadLine(Integer.parseInt(args[0]),args[1]);
+    }
+    public static void ReadLine(int line, String path) throws IOException {
+        try (Stream<String> all_lines = Files.lines(Paths.get(path))) {
+            String input = all_lines.skip(line).findFirst().get();
+            System.out.println(input);
+            String output = input.replace("test","weakling");
+            System.out.println(output);
+            WriteLine(line,path,output);
+
+        }catch (NoSuchElementException e){
+            System.out.println("Line not found");
+        }
+    }
+
+    public static void WriteLine(int line, String filePath, String newLine) throws IOException {
+        Path path = Paths.get(filePath);
+        List<String> lines = Files.readAllLines(path);
+        lines.set(line, newLine);
+        Files.write(path, lines);
     }
 }
